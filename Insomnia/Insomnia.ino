c676aa1ee3d2fb93e3644e26430d35c65500c9f3
LAPTOP-C284JHA0\hufer <huferry@gmail.com>
2021-01-16 12:05:38 +0100

    Added test. Made the movement smaller and faster.

--- Insomnia/Insomnia.ino
+++ Insomnia/Insomnia.ino
@@ -8,9 +8,9 @@ void setup() {
 
 void loop() {
   digitalWrite(1, HIGH);
-  DigiMouse.moveY(1);
-  DigiMouse.delay(500);
-  DigiMouse.moveY(-1);
+  DigiMouse.moveY(2);
+  DigiMouse.delay(100);
+  DigiMouse.moveY(-2);
   digitalWrite(1, LOW); 
   
   DigiMouse.delay(10000); // 1 sec delay
